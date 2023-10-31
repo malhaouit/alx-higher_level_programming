31a6c51d3a487ffa701ef1a795f45ecbfcf9c3a7
malhaouit <abdelmounaim.abdel@gmail.com>
2023-10-30 17:00:44 -0700

    Check the existance of a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks a singly linked lists if contains any cycle
+ * @list: A pointer to the first node
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+	int counter = 0;
+
+	current = list;
+
+	while (current != NULL)
+	{
+		current = current->next;
+
+		if (current == list)
+		{
+			counter++;
+			break;
+		}
+	}
+
+	if (counter)
+		return (1);
+
+	return (0);
+}
