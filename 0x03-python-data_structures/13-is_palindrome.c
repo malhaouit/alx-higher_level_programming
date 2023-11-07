ac788826838ee90216415cda620270e55182d717
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-06 17:27:01 -0800

    Check if a linkedlist is a palindrome or not

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * initialize_array - Initialize array by numbers in nodes
+ * @numbers: The dynamic array
+ * @current: A pointer to iterate over the list of nodes
+ *
+ * Return: The number of nodes
+ */
+int initialize_array(int *numbers, listint_t *current)
+{
+	int n = 0;
+
+	while (current != NULL)
+	{
+		numbers[n] = current->n;
+		current = current->next;
+		n++;
+	}
+
+	return (n);
+}
+
+/**
+ * is_palindrome - Checks if the linked list is palindrome or not
+ * @head: A pointer to pointer to the first node of the linked list
+ *
+ * Return: 1 if the likendlist is palindrome, or 0 if it is not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current;
+	int *numbers;
+	int n;
+
+	numbers = malloc(sizeof(int) * 1024);
+	current = *head;
+
+	n = initialize_array(numbers, current);
+
+	if (n == 0)
+	{
+		free(numbers);
+		return (1);
+	}
+
+	if (n % 2 == 0)
+	{
+		int i = 0, j = n - 1;
+
+		while (i < n / 2)
+		{
+			if (numbers[i++] != numbers[j--])
+			{
+				free(numbers);
+				return (0);
+			}
+		}
+	}
+	free(numbers);
+	return (1);
+}
