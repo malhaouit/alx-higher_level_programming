218c20d6aa2726bd104a5694318c29c80f530a31
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-08 12:44:52 -0800

    Square of matrix elements

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    if matrix:
+        new_matrix = []
+
+        for row in matrix:
+            new_row = []
+            for i in row:
+                new_row.append(i ** 2)
+            new_matrix.append(new_row)
+        return new_matrix
