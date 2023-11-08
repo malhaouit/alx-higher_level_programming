c010f7a4cd25fd3f365419d57ba46c19c00e9e02
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-08 04:56:29 -0800

    Add casting

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -11,8 +11,8 @@ void print_python_list_info(PyObject *p)
 	int i;
 	PyObject *plo;
 
-	printf("[*] Size of the Python List = %d\n", Py_SIZE(p));
-	printf("[*] Allocated = %d\n", ((PyListObject *)p)->allocated);
+	printf("[*] Size of the Python List = %d\n", (int)Py_SIZE(p));
+	printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
 
 	for (i = 0; i < Py_SIZE(p); i++)
 	{
