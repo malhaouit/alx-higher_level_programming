3b7b7267813e7506047344993cb6573a58cfaeca
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-08 13:36:53 -0800

    Reduce the code

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    if my_list:
-        uniques = set()
-        for num in my_list:
-            uniques.add(num)
-        
+        uniques = set(my_list)
         result = 0
+
         for element in uniques:
             result += element
         return result
