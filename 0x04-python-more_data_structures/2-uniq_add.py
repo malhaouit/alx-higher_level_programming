9c1d7f1b9862a13072d2a0c334092297c656eb66
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-08 13:39:53 -0800

    Fix over-indentatio

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-        uniques = set(my_list)
-        result = 0
+    uniques = set(my_list)
+    result = 0
 
-        for element in uniques:
-            result += element
-        return result
+    for element in uniques:
+        result += element
+    return result
