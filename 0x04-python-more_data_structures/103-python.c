f12f7131c85c0f5f3292a888abf886e580098b3c
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-11 14:12:13 -0800

    Print some basic info about Python lists and Python bytes objects

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,28 @@
+#include <Python.h>
+
+/**
+ * print_python_list - Prints some basic info about Python lists
+ * @p: Python object
+ *
+ * Return: Nothing
+ */
+void print_python_list(PyObject *p)
+{
+        int i;
+        PyObject *plo;
+
+        printf("[*] Size of the Python List = %d\n", (int)Py_SIZE(p));
+        printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
+
+        for (i = 0; i < Py_SIZE(p); i++)
+        {
+                printf("Element %d: ", i);
+                plo = PyList_GetItem(p, i);
+                printf("%s\n", Py_TYPE(plo)->tp_name);
+        }
+}
+
+void print_python_bytes(PyObject *p)
+{
+        (void)p;
+}
