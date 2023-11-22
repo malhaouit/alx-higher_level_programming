af49a5c41e8d5b111f90255ca92590fc3efa8fe9
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-22 12:17:17 -0800

    Updates: Update the function documt. - Fix the first condition

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-""" Square class, no module imported """
+""" Square class, no module imported"""
 
 
 class Square:
     """ Square class that defines a square """
 
     def __init__(self, size=0):
-        self.__size = size
-    """ value instantiation of size attribute """
-
-    if type(size) != int:
-        raise TypeError("size must be an integer")
-
-    if size < 0:
-        raise ValueError("size must be >= 0")
+        """
+        initialize size value
+        args:
+        size (int): the size of square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
