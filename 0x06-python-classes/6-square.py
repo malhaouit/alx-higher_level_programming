9eb5b041a871531dd3a79b27dad3267bf20dc97e
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-22 14:53:43 -0800

    Add one more attribute and print the square with # and coordinate spaces

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+""" Square class, no module imported"""
+
+
+class Square:
+    """ Square class that defines a square """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        initialize size value
+        args:
+        size (int): the size of square
+        position (tuple): a tuple that represent coordinates
+        of a square
+        """
+        self.__size = size
+        self.position = position
+
+    def area(self):
+        """
+        calculate the area of the square
+        return: the area calculated
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        gets the value of size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the value of size
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """ Prints the square with character # """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                print()
+
+    @property
+    def position(self):
+        """ gets the position """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ sets the position """
+        print("hello")
+        if len(value) != 2 or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
