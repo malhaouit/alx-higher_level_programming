aaa34a0bff678790579b1cc49ad9f814bb566a47
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-22 11:42:43 -0800

    Value initiation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Square class, no module imported """
+
+
+class Square:
+    """ Square class that defines a square """
+
+    def __init__(self, value):
+        self.__size = value
+    """ value instantiation of size attribute """
