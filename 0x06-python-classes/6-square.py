1ccb283a536431de236a9ac469fe92ba5fea41be
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-22 14:58:05 -0800

    Remove additional Hello for testing... lol :)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -62,7 +62,6 @@ class Square:
     @position.setter
     def position(self, value):
         """ sets the position """
-        print("hello")
         if len(value) != 2 or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
