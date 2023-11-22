e5d3ec81bb18bf640a5ee30ce0e0a8711169852f
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-22 12:19:40 -0800

    Update: Fix the second conditon clause

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,6 +14,6 @@ class Square:
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise TypeError("size must be an integer")
+            raise TypeError("size must be >= 0")
         else:
             self.__size = size
