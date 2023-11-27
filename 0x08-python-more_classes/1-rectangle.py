4b7a6647c65fc91f7a0877d0ab7810c0a04ff2fb
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-27 12:20:56 -0800

    Update: Remove the two underscores before the attributes inside __init__

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Define and initialize width and height """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
