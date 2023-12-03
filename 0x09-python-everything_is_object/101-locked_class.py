574c5d700d69f47be56bfd027836f3fbb331c785
malhaouit <abdelmounaim.abdel@gmail.com>
2023-12-03 05:27:38 -0800

    Add documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+""" This module cover a Locked Class, no module imported """
+
+
 class LockedClass:
+    """
+    This a class with no class or object attribute and prevents any creation
+    of a new instance attributes, except if the new instance attribute
+    is called `first_name`
+    """
+
     __slots__ = 'first_name',
