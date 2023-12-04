52904dd8caacfc8f421f248b54ae98da6098d593
malhaouit <abdelmounaim.abdel@gmail.com>
2023-12-04 06:13:47 -0800

    Improve Geometry: raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module based on the prvious module 5-base_geometry.py
+New function added, and module imported """
+
+
+class BaseGeometry:
+    """ This BaseGeometry includes one handler function """
+    def area(self):
+        """ Raises an exception """
+        raise Exception("area() is not implemented")
