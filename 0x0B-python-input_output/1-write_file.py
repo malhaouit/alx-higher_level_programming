e799f8b664c7283a7b181f044f24b0e2ebaccbcf
malhaouit <abdelmounaim.abdel@gmail.com>
2023-12-05 10:34:23 -0800

    Write a text into a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module has one function, and no extra module imported """
+
+
+def write_file(filename="", text=""):
+    """ Writes a text into the filename """
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
