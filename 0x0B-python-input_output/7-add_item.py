3eef2f6caf99fd277408803ee31b5b81f62cac75
malhaouit <abdelmounaim.abdel@gmail.com>
2023-12-06 11:46:33 -0800

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""This module loads a list, saves the updated list to the JSON file"""
+"""This module loads a list, saves the updated list to a JSON file"""
 from sys import argv
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
 
-arg_list = load_from_json_file('add_item.json')
+try:
+    arg_list = load_file('add_item.json')
+except (ValueError, FileNotFoundError):
+    arg_list = []
 
-for arg in argv[1:]:
-    arg_list.append(arg)
+for item in argv[1:]:
+    arg_list.append(item)
 
-save_to_json_file(arg_list, 'add_item.json')
+save_file(arg_list, 'add_item.json')
