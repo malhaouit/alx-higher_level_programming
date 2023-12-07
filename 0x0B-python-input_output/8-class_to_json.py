cdedb1f7b9287635524e682fb7f9f7dc2c966292
malhaouit <abdelmounaim.abdel@gmail.com>
2023-12-07 03:52:38 -0800

    Update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,9 @@
-#!/bin/usr/python3
-""" Returns the dictionary description for JSON serialization of an object """
+#!/usr/bin/python3
+"""
+file: 8-class_to_json.py
+functions:
+-> class_to_json
+"""
 
 
 def class_to_json(obj):
