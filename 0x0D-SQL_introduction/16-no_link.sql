3699a2eb4f035fb2fb5786fb7e0dca22715ae7b6
malhaouit <abdelmounaim.abdel@gmail.com>
2024-01-16 14:50:00 -0800

    List all records of the table with specifications

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- A script that lists all records of the table second_table of the database hbtn_0c_0 in MySQL server:
+-- Rows listed with a name value
+-- Results display the score and the name (in this order)
+-- Records listed by descending score
+-- The database name will be passed as an argument to the mysql command
+SELECT score, name FROM second_table ORDER BY score DESC
