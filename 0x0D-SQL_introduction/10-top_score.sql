61b3e7f2abfec6f7f907eac30d78109c7b1a4279
malhaouit <abdelmounaim.abdel@gmail.com>
2024-01-16 13:56:57 -0800

    Select specific columns records in descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- A script that lists all records of the table second_table of the database hbtn_0c_0 in MySQL server:
+-- Display score and name
+-- Records should be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC
