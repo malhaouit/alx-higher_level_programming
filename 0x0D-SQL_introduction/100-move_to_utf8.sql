99932346522d440713fc8dae307537d5ea43fa10
malhaouit <abdelmounaim.abdel@gmail.com>
2024-01-16 15:51:38 -0800

    Move `USE database_name` to other line

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,8 @@
 -- Converts database hbtn_0c_0, first_table and name to UTF-8.
-USE hbtn_0c_0;
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table MODIFY COLUMN name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
