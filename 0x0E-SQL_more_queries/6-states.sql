c719920e2e504071b5de7abf844e4460ed21501f
malhaouit <abdelmounaim.abdel@gmail.com>
2024-01-17 11:23:27 -0800

    Create new database and table, the id is auto-generated

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Creates the database `hbtn_0d_usa` and the table `states`.
+-- In table states: the `id` is unique, auto generated, can’t be null and is a primary key.
+-- In table states: the `name` can't be null.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(256));
