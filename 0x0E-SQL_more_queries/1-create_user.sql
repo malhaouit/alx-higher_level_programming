ad7501e448bf05da69492f16d1e5aaef1978cb2d
malhaouit <abdelmounaim.abdel@gmail.com>
2024-01-17 09:34:10 -0800

    Create a user if not exists and grant all priviliges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates the MySQL server user user_0d_1 and grants all privileges on MySQL server.
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
