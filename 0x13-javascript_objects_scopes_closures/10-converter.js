3e26dd6e469daa00f8e186623c464366e7b9c68e
malhaouit <abdelmounaim.abdel@gmail.com>
2024-02-14 14:37:20 -0800

    Update: add base 2 coversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -4,6 +4,8 @@ exports.converter = function (base) {
   return function (x) {
     if (base === 16) {
       return x.toString(16);
+    } else if (base === 2) {
+      return x.toString(2);
     }
     return x;
   };
