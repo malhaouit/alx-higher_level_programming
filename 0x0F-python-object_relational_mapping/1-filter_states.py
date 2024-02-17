2063bd2c09a3139bd049ca875c9df27431d9c17c
malhaouit <abdelmounaim.abdel@gmail.com>
2024-02-17 13:57:00 -0800

    Update the query using BINARY keyword and reduce the split the number of characters in one line

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,9 @@ if __name__ == "__main__":
             port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cur.execute(query)
+
     rows = cur.fetchall()
     for row in rows:
         print(row)
