83dbb71a0764c35886270cee9ee9fea8f3a05e3e
malhaouit <abdelmounaim.abdel@gmail.com>
2024-02-18 14:32:27 -0800

    A script that prints the id of state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A script that prints the State object with
+the name passed as argument from the database.
+"""
+
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    connection_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                    mysql_username, mysql_password, database_name)
+
+    engine = create_engine(connection_string, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(name=state_name).one()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
