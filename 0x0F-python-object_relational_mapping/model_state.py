20dedce59117fac52374421b204194e0e6972b96
malhaouit <abdelmounaim.abdel@gmail.com>
2024-02-17 18:07:38 -0800

    Update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,3 +14,8 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
