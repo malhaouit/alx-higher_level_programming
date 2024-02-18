1432cebc9f04967ebc6ed3fd9e6cc5e48697bf56
malhaouit <abdelmounaim.abdel@gmail.com>
2024-02-18 07:05:31 -0800

    A script that lists all states - a State model is used

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that lists all State objects from the database.
+Look at the file: model_state.py for more information.
+"""
+
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    connection_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                    mysql_username, mysql_password, database_name)
+
+    engine = create_engine(connection_string, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
+
+    session.close()
