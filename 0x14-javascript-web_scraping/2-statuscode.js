5b92d5201a38c60136f50ec77fd03a91287fbe21
malhaouit <abdelmounaim.abdel@gmail.com>
2024-03-26 15:10:03 -0700

    A script that prints the title of a specific Star Wars movie

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const args = process.argv;
+const URL = args[2];
+request
+  .get(URL)
+  .on('response', function (response) {
+    console.log(`code: ${response.statusCode}`);
+  });
